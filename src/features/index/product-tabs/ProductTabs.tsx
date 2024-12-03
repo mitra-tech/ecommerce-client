6e2f6e53f0496aef79cff549955b386e60e06101
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-03 12:42:53 -0700

    add: Items to product tabs

--- src/features/index/product-tabs/ProductTabs.tsx
+++ src/features/index/product-tabs/ProductTabs.tsx
@@ -1,12 +1,13 @@
-import { FC, ReactElement} from 'react';
-import { categories } from 'src/features/shared/utils/utils.service';
+import { FC, ReactElement, useState } from 'react';
+import { categories, replaceSpacesWithDash } from 'src/features/shared/utils/utils.service';
 import { v4 as uuidv4 } from 'uuid';
 
-
 const ProductTabs: FC = (): ReactElement => {
+  const [activeTab, setActiveTab] = useState<string>('Graphics & Design');
+  let categoryProducts = [];
 
   return (
-      <div className="relative m-auto mt-8 w-screen px-6 xl:container md:px-12 lg:px-6">
+    <div className="relative m-auto mt-8 w-screen px-6 xl:container md:px-12 lg:px-6">
       <div className="mx-auto flex flex-col px-4 py-8 lg:px-6 lg:py-10">
         <div className="flex flex-col text-left">
           <h2 className="mb-3 text-3xl font-bold text-black">A broad selection of services</h2>
@@ -17,11 +18,27 @@ const ProductTabs: FC = (): ReactElement => {
             {categories().map((category: string) => (
               <li
                 key={uuidv4()}
+                onClick={() => setActiveTab(category)}
+                className={`cursor-pointer font-bold py-2 lg:py-0 ${activeTab === category ? 'text-black' : 'text-gray-400'}`}
               >
                 {category}
               </li>
             ))}
           </ul>
+        </div>
+        <div className="mt-4 h-full overflow-hidden border px-6 py-6">
+          {categoryProducts.length > 0 ? (
+            <>
+              <a
+                className="mt-10 w-[10%] rounded border border-black px-6 py-3 text-center text-sm font-bold text-black hover:bg-gray-100 focus:outline-none md:px-4 md:py-2 md:text-base"
+                href={`/search/categories/${replaceSpacesWithDash(activeTab)}`}
+              >
+                Explore
+              </a>
+            </>
+          ) : (
+            <div className="flex h-96 items-center justify-center text-lg">Information not available at the moment.</div>
+          )}
         </div>
       </div>
     </div>
