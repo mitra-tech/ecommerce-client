8509aa40c398c1ae9c066eed01c2d8e0a0a9b624
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-03 13:33:06 -0700

    add: categories and howitworks components

--- src/features/index/Index.tsx
+++ src/features/index/Index.tsx
@@ -2,6 +2,8 @@ import { FC, lazy, LazyExoticComponent, ReactElement, Suspense } from 'react';
 import { IHeader } from '../shared/header/interfaces/header.interface';
 import Hero from './Hero';
 import ProductTabs from './product-tabs/ProductTabs';
+import HowItWorks from './HowItWorks';
+import Categories from './Categories';
 
 const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('../shared/header/components/Header'));
 
@@ -13,6 +15,8 @@ const Index: FC = (): ReactElement => {
         <IndexHeader navClass="navbar peer-checked:navbar-active fixed z-20 w-full border-b border-gray-100 bg-white/90 shadow-2xl shadow-gray-600/5 backdrop-blur dark:border-gray-800 dark:bg-gray-900/80 dark:shadow-none" />
         <Hero />
         <ProductTabs/>
+        <HowItWorks/>
+        <Categories />
       </Suspense>
     </div>
   );
