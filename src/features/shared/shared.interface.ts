d5fcb523ae3aa80770d766c80f12075922135908
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-02 17:01:34 -0700

    remove: Shared libarary. maybe you wonder, why? Mitra: Well, some dependencies in the shared library work on node env not a browser env, haha!

--- src/features/shared/shared.interface.ts
+++ src/features/shared/shared.interface.ts
@@ -1,62 +1,20 @@
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { ChangeEvent, CSSProperties, Dispatch, KeyboardEvent, ReactNode, SetStateAction } from 'react';
-import {
-  IAuthDocument,
-  IBuyerDocument,
-  ISellerDocument,
-  ISellerGig,
-  IAuthResponse,
-  IMessageDocument,
-  IOrderDocument,
-  IReviewDocument,
-  IOrderNotifcation,
-  ISignInPayload,
-  ISignUpPayload,
-  IResetPassword,
-  ICreateGig,
-  IExperience,
-  IEducation,
-  ILanguage
-} from "@mitra-tech/ecommerce_microservice_app"
 
-export type validationErrorsType =
-  | ISignInPayload
-  | ISignUpPayload
-  | IResetPassword
-  | ICreateGig
-  | 'IPersonalInfoData'
-  | IExperience
-  | IEducation
-  | ILanguage;
 
 export interface IQueryResponse {
-  data: IAuthResponse;
   error: FetchBaseQueryError | SerializedError;
 }
 
 export interface IResponse {
   message?: string;
   token?: string;
-  user?: IAuthDocument;
-  buyer?: IBuyerDocument;
-  seller?: ISellerDocument;
-  sellers?: ISellerDocument[];
-  gig?: ISellerGig;
-  gigs?: ISellerGig[];
   total?: number;
   sortItems?: string[];
-  conversations?: 'IConversationDocument[]' | IMessageDocument[];
-  messages?: IMessageDocument[];
-  messageData?: IMessageDocument;
   conversationId?: string;
   clientSecret?: string;
   paymentIntentId?: string;
-  order?: IOrderDocument;
-  orders?: IOrderDocument[];
-  review?: IReviewDocument;
-  reviews?: IReviewDocument[];
-  notifications?: IOrderNotifcation[];
   browserName?: string;
   deviceType?: string;
 }
