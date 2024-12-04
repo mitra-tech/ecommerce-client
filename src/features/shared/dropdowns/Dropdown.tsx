b0a5cc04e7525e86a3d8d25ed40799e3a42509d0
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-03 17:24:03 -0700

    add: alert and dropdown

---
+++ src/features/shared/dropdowns/Dropdown.tsx
@@ -0,0 +1,17 @@
+
+import {  FC, ReactElement } from 'react';
+
+import { IDropdownProps } from '../shared.interface';
+
+const Dropdown: FC<IDropdownProps> = ({
+
+}): ReactElement => {
+
+  return (
+    <div >
+
+    </div>
+  );
+};
+
+export default Dropdown;
