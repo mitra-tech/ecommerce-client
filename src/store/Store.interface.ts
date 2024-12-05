d865a83a60a47a79fbabacdcf18efabf06315ebe
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-04 17:53:13 -0700

    add: Redux store

---
+++ src/store/Store.interface.ts
@@ -0,0 +1,11 @@
+
+
+export interface IReduxState {
+  authUser: object;
+  header: string;
+  logout: boolean;
+  buyer: object;
+  seller: object;
+  showCategoryContainer: boolean;
+  notification: object;
+}
---
+++ src/store/Store.ts
@@ -0,0 +1,22 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { Reducer } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  blacklist: ['clientApi', '_persist']
+};
+
+export const combineReducer = combineReducers({});
+
+export const rootReducers: Reducer<RootState> = (state, action) => {
+  // this is to reset the state to default when user logs out
+  if (action.type === 'logout/logout') {
+    state = {} as RootState;
+  }
+  return combineReducer(state, action);
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
