faa846ce75e05f424a59a72bff7a706c66c6f83c
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-05 18:20:17 -0700

    add: auth object schema with Yup

--- src/store/Store.interface.ts
+++ src/store/Store.interface.ts
@@ -1,7 +1,8 @@
+import { IAuthUser } from "src/features/auth/interfaces/auth.interface";
 
 
 export interface IReduxState {
-  authUser: object;
+  authUser: IAuthUser;
   header: string;
   logout: boolean;
   buyer: object;
