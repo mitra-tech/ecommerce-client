0be1272f9fe14e8bc6171e3b1fa2b243895ca424
mitra-tech <davoudabadi.mitra@gmail.com>
2024-12-13 16:30:03 -0700

    add: check current user query

--- src/features/AppPage.tsx
+++ src/features/AppPage.tsx
@@ -1,12 +1,59 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useCallback, useEffect, useState } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import HomeHeader from 'src/shared/header/components/HomeHeader';
+import { getDataFromLocalStorage, saveToSessionStorage } from '../shared/utils/utils.service';
+import { useAppDispatch, useAppSelector } from '../store/Store';
+import { IReduxState } from '../store/Store.interface';
+import { addAuthUser } from './auth/reducers/auth.reducer';
+import { useCheckCurrentUserQuery } from './auth/services/auth.service';
+import Home from '../features/home/Home';
 import Index from './index/Index';
 
-export const AppPage: FC = (): ReactElement => {
-  return (
-    <div>
+const AppPage: FC = (): ReactElement => {
+  const authUser = useAppSelector((state: IReduxState) => state.authUser);
+  const appLogout = useAppSelector((state: IReduxState) => state.logout);
+  const showCategoryContainer = useAppSelector((state: IReduxState) => state.showCategoryContainer);
+  const [tokenIsValid, setTokenIsValid] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const { data: currentUserData, isError } = useCheckCurrentUserQuery(undefined, { skip: authUser.id === null });
+
+  const checkUser = useCallback(async () => {
+    try {
+      if (currentUserData && currentUserData.user && !appLogout) {
+        setTokenIsValid(true);
+        dispatch(addAuthUser({ authInfo: currentUserData.user }));
+        saveToSessionStorage(JSON.stringify(true), JSON.stringify(authUser.username));
+
+        // dispatch buyer info
+        // dispatch seller info
+
+        if (authUser.username !== null) {
+          console.log('add socket');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [currentUserData, dispatch, appLogout, authUser.username]);
+
+  useEffect(() => {
+    checkUser();
+  }, [checkUser]);
+
+  if (authUser) {
+    return !tokenIsValid && !authUser.id ? (
       <Index />
-    </div>
-  );
+    ) : (
+      <>
+        <>
+          <HomeHeader showCategoryContainer={showCategoryContainer} />
+          <Home />
+        </>
+      </>
+    );
+  } else {
+    return <Index />;
+  }
 };
 
 export default AppPage;
