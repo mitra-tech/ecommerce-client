1cefacbe261ec6278e8fc01f5038f9ff102870a9
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-02 16:42:17 -0700

    add: seller personal info component

--- src/features/sellers/components/add/AddSeller.tsx
+++ src/features/sellers/components/add/AddSeller.tsx
@@ -1,17 +1,21 @@
 import { FC, ReactElement } from 'react';
 
 import Breadcrumb from 'src/shared/breadcrumbs/Breadcrumbs';
+import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
 import { useAppSelector } from 'src/store/Store';
 import { IReduxState } from 'src/store/Store.interface';
 
 const AddSeller: FC = (): ReactElement => {
   const authUser = useAppSelector((state: IReduxState) => state.authUser);
+  const isLoading = false;
 
   return (
     <div className="relative w-full">
       <Breadcrumb breadCrumbItems={['Seller', 'Create Profile']} />
       <div className="container mx-auto my-5 overflow-hidden px-2 pb-12 md:px-0">
-        {/* <add circular loder here> */}
+        {isLoading && (
+          <CircularPageLoader/>
+        )}
         {authUser && !authUser.emailVerified && (
           <div className="absolute left-0 top-0 z-50 flex h-full w-full justify-center bg-white/[0.8]">
             <span className="mt-20">Please verify your email</span>
--- src/features/sellers/interfaces/seller.interfaces.ts
+++ src/features/sellers/interfaces/seller.interfaces.ts
@@ -1,5 +1,8 @@
 // By extending ISellerDocument with the Record<string, any> we allow an object to contain other
 // string keys with any values along with those defined in the interface.
+
+import { Dispatch, SetStateAction } from "react";
+
 // The nice part is that you still have the autocompletion for the defined properties
 export type SellerType = string | string[] | number | Date | unknown | undefined;
 
@@ -31,4 +34,18 @@ export interface ISellerDocument extends Record<string, SellerType> {
   totalGigs?: number;
   paypal?: string; // not needed
   createdAt?: Date | string;
+}
+
+export interface IPersonalInfoData {
+  [key: string]: string;
+  fullName: string;
+  profilePicture: string;
+  description: string;
+  responseTime: string;
+  oneliner: string;
+}
+export interface IPersonalInfoProps {
+  personalInfo: IPersonalInfoData;
+  setPersonalInfo: Dispatch<SetStateAction<IPersonalInfoData>>;
+  personalInfoErrors: IPersonalInfoData[];
 }
