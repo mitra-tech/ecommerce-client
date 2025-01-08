854f94f072c12a6b29147c29fe69381bdeca9260
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-08 12:06:11 -0700

    add: timeago methods

--- src/shared/utils/image-utils.service.ts
+++ src/shared/utils/image-utils.service.ts
@@ -1,95 +1,134 @@
-// A file for image and video validation
+import { differenceInCalendarDays, format, getISOWeek, isSameDay, subDays } from 'date-fns';
 
-export const validateImage = (file: File, type: string): boolean => {
-  if (type === 'image') {
-    const validImageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
-    return file && validImageTypes.indexOf(file.type) > -1;
-  } else {
-    const validVideoTypes = ['video/m4v', 'video/avi', 'video/mpg', 'video/mp4', 'video/webm'];
-    return file && validVideoTypes.indexOf(file.type) > -1;
-  }
-};
-
-export const checkImageSize = (file: File, type: string): string => {
-  let fileError = '';
-  const isValid = validateImage(file, type);
-  if (!isValid) {
-    fileError = `File ${file.name} not accepted`;
-  }
-  if (file.size > 50000000) {
-    // 50 MB
-    fileError = 'File is too large.';
-  }
-  return fileError;
-};
-
-export const checkImage = (file: File, type: string): boolean => {
-  let isValid = true;
-  if (!validateImage(file, type)) {
-    window.alert(`File ${file.name} not accepted`);
-    isValid = false;
-    return false;
-  }
-  if (checkImageSize(file, type)) {
-    window.alert(checkImageSize(file, type));
-    isValid = false;
-    return false;
-  }
-  return isValid;
-};
-
-export const checkFile = (file: File): boolean => {
-  let hasError = false;
-  if (file.size > 1000000000) {
-    // 1 GB
-    window.alert('File is too large. Max. size is 1 GB.');
-    hasError = true;
-  }
-  if (!file.name.match(/\.(jpg|jpeg|png|gif|pdf|webp|zip|m4v|avi|mpg|mp4|webm)$/)) {
-    window.alert('Only files of type jpg, jpeg, png, gif or pdf are allowed');
-    hasError = true;
-  }
-  return hasError;
-};
-
-export const checkUrlExtension = (fileExtension: string): string => {
-  let fileType = '';
-  if (['jpg', 'jpeg', 'png', 'gif', 'webp'].includes(fileExtension)) {
-    fileType = 'image';
+export class TimeAgo {
+  static transform(value: string | number | Date): string {
+    if (!value) {
+      return '';
+    }
+    const date = typeof value === 'string' ? new Date(value) : value;
+    return this.timeDifference(new Date(), new Date(date));
   }
 
-  if (['pdf'].includes(fileExtension)) {
-    fileType = 'pdf';
+  static chatMessageTransform(value: string | number | Date): string {
+    if (!value) {
+      return '';
+    }
+    const date = typeof value === 'string' ? new Date(value) : value;
+    const yesterday = subDays(new Date(), 1);
+    if (isSameDay(date, new Date())) {
+      return 'Today';
+    } else if (isSameDay(date, yesterday)) {
+      return 'Yesterday';
+    } else if (getISOWeek(new Date()) === getISOWeek(date) || getISOWeek(new Date()) - getISOWeek(date) === 1) {
+      return format(date, 'EEEE');
+    } else {
+      return format(date, 'd MMMM, yyyy');
+    }
   }
 
-  if (['m4v', 'avi', 'mpg', 'mp4', 'webm'].includes(fileExtension)) {
-    fileType = 'video';
+  static formatDateToMonthAndYear(value: string): string {
+    if (!value) {
+      return '';
+    }
+    const date: Date = new Date(value);
+    return format(date, 'MMMM, yyyy');
   }
 
-  if (['zip'].includes(fileExtension)) {
-    fileType = 'zip';
+  static dayMonthYear(value: string): string {
+    const date: Date = new Date(value);
+    return format(date, 'd MMMM, yyyy');
   }
-  return fileType;
-};
 
-export const fileType = (file: File): string => {
-  const list: string[] = file.name.split('.');
-  const fileType: string = list[list.length - 1];
-  return fileType;
-};
+  static dayWithTime(value: string): string {
+    const date: Date = new Date(value);
+    return `${format(date, 'd MMM')} at ${format(date, 'HH:mm')}`;
+  }
 
-export const readAsBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
-  const reader: FileReader = new FileReader();
-  const fileValue: Promise<string | ArrayBuffer | null> = new Promise((resolve, reject) => {
-    reader.addEventListener('load', () => {
-      resolve(reader.result);
-    });
+  static timeFormat(value: string): string {
+    const date: Date = new Date(value);
+    return format(date, 'HH:mm a');
+  }
 
-    reader.addEventListener('error', (event: ProgressEvent<FileReader>) => {
-      reject(event);
-    });
+  static compareDates(date1: string, date2: string): number {
+    const firstDate: string = format(new Date(date1), 'd/MM/yyyy');
+    const secondDate: string = format(new Date(date2), 'd/MM/yyyy');
+    if (firstDate > secondDate) {
+      return -1;
+    } else if (firstDate < secondDate) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 
-    reader.readAsDataURL(file);
-  });
-  return fileValue;
-};
+  static dateInDays(date: string): string {
+    let result = '';
+    const difference: number = differenceInCalendarDays(new Date(), new Date(date));
+    const months: number = Math.floor(difference / 30);
+    if (months <= 0) {
+      const weeks: number = Math.floor(difference / 7);
+      result = weeks >= 1 ? `${weeks} week${weeks >= 2 ? 's' : ''}` : `${difference} day${difference >= 2 ? 's' : ''}`;
+    } else if (months === 1) {
+      result = `${months} month`;
+    } else {
+      result = `${months} months`;
+    }
+    return result;
+  }
+
+  static timeDifference(current: number | Date, date: number | Date): string {
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const elapsed = current.valueOf() - date.valueOf();
+
+    if (format(current, 'yyyy') === format(date, 'yyyy')) {
+      if (elapsed < msPerMinute) {
+        return this.secondsAgo(elapsed);
+      } else if (elapsed < msPerHour) {
+        return this.minutesAgo(elapsed, msPerMinute);
+      } else if (elapsed < msPerDay) {
+        return this.hoursAgo(elapsed, msPerHour);
+      } else if (elapsed < msPerMonth) {
+        return this.monthsAgo(date, elapsed, msPerDay);
+      } else {
+        return format(date, 'MMM d');
+      }
+    } else {
+      return format(date, 'MMM d, yyyy');
+    }
+  }
+
+  static secondsAgo(elapsed: number): string {
+    if (Math.round(elapsed / 1000) <= 1) {
+      return 'a second ago';
+    } else {
+      return `${Math.round(elapsed / 1000)} seconds ago`;
+    }
+  }
+
+  static minutesAgo(elapsed: number, msPerMinute: number): string {
+    if (Math.round(elapsed / msPerMinute) <= 1) {
+      return 'a minute ago';
+    } else {
+      return `${Math.round(elapsed / msPerMinute)} minutes ago`;
+    }
+  }
+
+  static hoursAgo(elapsed: number, msPerHour: number): string {
+    if (Math.round(elapsed / msPerHour) <= 1) {
+      return 'an hour ago';
+    } else {
+      return `${Math.round(elapsed / msPerHour)} hours ago`;
+    }
+  }
+
+  static monthsAgo(date: number | Date, elapsed: number, msPerDay: number): string {
+    if (Math.round(elapsed / msPerDay) <= 7) {
+      return format(date, 'eeee');
+    } else {
+      return format(date, 'MMM d');
+    }
+  }
+}
