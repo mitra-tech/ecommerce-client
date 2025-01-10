6f672082cdb77509332c0e1e623c2c8829e89460
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-10 10:12:35 -0700

    add: seller socil links

--- src/features/sellers/components/profile/components/overview/socaillinks/SocielLinks.tsx
+++ src/features/sellers/components/profile/components/overview/socaillinks/SocielLinks.tsx
@@ -3,7 +3,7 @@ import { FaPencilAlt } from 'react-icons/fa';
 import { SellerContext } from 'src/features/sellers/context/SellerContext';
 import { v4 as uuidv4 } from 'uuid';
 
-import SocialLinksEditField from './SocialLinksEditField';
+import SocialLinksEditField from './SocialLinksEditFields';
 
 const SocialLinks: FC = (): ReactElement => {
   const [showSocialLinkAddForm, setShowSocialLinkAddForm] = useState<boolean>(false);
