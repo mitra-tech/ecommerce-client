c1f14eda6df6e3bcf2cc8b9c2f854f70680980b6
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-16 12:05:17 -0700

    add: suspense component fallback to the Index file

--- src/features/index/Index.tsx
+++ src/features/index/Index.tsx
@@ -1,21 +1,29 @@
-import { FC, lazy, LazyExoticComponent, ReactElement, Suspense } from 'react';
-import { IHeader } from '../../shared/header/interfaces/header.interface';
-import Hero from './Hero';
-import ProductTabs from './product-tabs/ProductTabs';
-import HowItWorks from './HowItWorks';
+import { FC, lazy, LazyExoticComponent, ReactElement, Suspense, useEffect } from 'react';
+import { IHeader } from 'src/shared/header/interfaces/header.interface';
+import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
+import { saveToSessionStorage } from 'src/shared/utils/utils.service';
+
 import Categories from './Categories';
+import GigTabs from './gig-tabs/GigTabs';
+import Hero from './Hero';
+import HowItWorks from './HowItWorks';
 
-const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('../../shared/header/components/Header'));
-
+const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('src/shared/header/components/Header'));
 
 const Index: FC = (): ReactElement => {
+  useEffect(() => {
+    saveToSessionStorage(JSON.stringify(false), JSON.stringify(''));
+  }, []);
+
   return (
     <div className="flex flex-col">
-      <Suspense>
+      {/* If Suspense needs a fallback it is going to use the CircularPageLoader */}
+      <Suspense fallback={<CircularPageLoader />}>
         <IndexHeader navClass="navbar peer-checked:navbar-active fixed z-20 w-full border-b border-gray-100 bg-white/90 shadow-2xl shadow-gray-600/5 backdrop-blur dark:border-gray-800 dark:bg-gray-900/80 dark:shadow-none" />
         <Hero />
-        <ProductTabs/>
-        <HowItWorks/>
+        <GigTabs />
+        <HowItWorks />
+        <hr />
         <Categories />
       </Suspense>
     </div>
