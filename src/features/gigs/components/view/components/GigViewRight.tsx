7013b734c3443375519bd5ee6880ee613a335bbb
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-22 10:34:19 -0700

    add: related tags component

--- src/features/gigs/components/view/components/GigViewRight.tsx
+++ src/features/gigs/components/view/components/GigViewRight.tsx
@@ -2,12 +2,14 @@ import { FC, ReactElement } from 'react';
 
 import GigPackage from './gigViewRight/GigPackage';
 import GigSeller from './gigViewRight/GigSeller';
+import GigRelatedTags from './gigViewRight/GigRelatedTags';
 
 const GigViewRight: FC = (): ReactElement => {
   return (
     <>
       <GigPackage />
       <GigSeller />
+      <GigRelatedTags />
     </>
   );
 };
