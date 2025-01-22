a966e73f452897c152dd4917dd794dbc80be3b00
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-22 12:34:37 -0700

    add: git left about componetn

--- src/features/gigs/components/view/components/GigViewLeft.tsx
+++ src/features/gigs/components/view/components/GigViewLeft.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactElement } from 'react';
 import GigLeftOverview from './gigViewLeft/GigLeftOverview';
+import GigLeftAbout from './gigViewLeft/GigLeftAbout';
 
 const GigViewLeft: FC = (): ReactElement => {
   return (
     <>
       <GigLeftOverview />
+      <GigLeftAbout />
     </>
   );
 };
