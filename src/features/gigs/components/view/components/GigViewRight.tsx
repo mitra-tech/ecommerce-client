8f404864d7cd6c05ec255fb7ae23349c031e2542
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-22 09:53:55 -0700

    add: gig view right

---
+++ src/features/gigs/components/view/components/GigViewRight.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactElement } from 'react';
+
+import GigPackage from './gigViewRight/GigPackage';
+
+const GigViewRight: FC = (): ReactElement => {
+  return (
+    <>
+      <GigPackage />
+
+    </>
+  );
+};
+
+export default GigViewRight;
