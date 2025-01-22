ddd6e0c23564d1b25cc398014025c8e76e7216b6
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-22 10:17:07 -0700

    add: gig seller profile

--- src/features/gigs/components/view/components/GigViewRight.tsx
+++ src/features/gigs/components/view/components/GigViewRight.tsx
@@ -1,12 +1,13 @@
 import { FC, ReactElement } from 'react';
 
 import GigPackage from './gigViewRight/GigPackage';
+import GigSeller from './gigViewRight/GigSeller';
 
 const GigViewRight: FC = (): ReactElement => {
   return (
     <>
       <GigPackage />
-
+      <GigSeller />
     </>
   );
 };
