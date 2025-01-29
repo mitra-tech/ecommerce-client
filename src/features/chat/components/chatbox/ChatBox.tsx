017a594ea75b9aaf84b263b18235e047b8a9347b
mitra-tech <davoudabadi.mitra@gmail.com>
2025-01-29 11:31:17 -0700

    add: chatbox interfaces and skeleton

---
+++ src/features/chat/components/chatbox/ChatBox.tsx
@@ -0,0 +1,8 @@
+import { FC, ReactElement } from 'react';
+import { IChatBoxProps } from '../../interfaces/chat.interfaces';
+
+const ChatBox: FC<IChatBoxProps> = (): ReactElement => {
+  return <div>ChatBox</div>;
+};
+
+export default ChatBox;
