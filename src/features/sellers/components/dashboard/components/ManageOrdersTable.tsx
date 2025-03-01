1cfd4f1f3e5484f8c4fd0e6b48740904420eee54
mitra-tech <davoudabadi.mitra@gmail.com>
2025-03-01 13:55:35 -0700

    update: manage orders table

--- src/features/sellers/components/dashboard/components/ManageOrdersTable.tsx
+++ src/features/sellers/components/dashboard/components/ManageOrdersTable.tsx
@@ -1,18 +1,47 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { IOrderDocument, IOrderTableProps } from 'src/features/order/interfaces/order.interfaces';
+import { IOrderDocument, IOrderMessage, IOrderTableProps } from 'src/features/order/interfaces/order.interfaces';
+import { useCancelOrderMutation } from 'src/features/order/services/order.service';
 import Button from 'src/shared/button/Button';
 import { updateHeader } from 'src/shared/header/reducers/header.reducer';
+import ApprovalModal from 'src/shared/modals/ApprovalModal';
+import { IApprovalModalContent } from 'src/shared/modals/interfaces/modal.interface';
 import { TimeAgo } from 'src/shared/utils/timeago.utils';
-import { lowerCase } from 'src/shared/utils/utils.service';
+import { lowerCase, showErrorToast, showSuccessToast } from 'src/shared/utils/utils.service';
 import { useAppDispatch } from 'src/store/store';
 import { v4 as uuidv4 } from 'uuid';
 
 const ManageOrdersTable: FC<IOrderTableProps> = ({ type, orders, orderTypes }): ReactElement => {
   const dispatch = useAppDispatch();
+  const [approvalModalContent, setApprovalModalContent] = useState<IApprovalModalContent>();
+  const [showCancelModal, setShowCancelModal] = useState<boolean>(false);
+  const selectedOrder = useRef<IOrderDocument>();
+  const [cancelOrder] = useCancelOrderMutation();
+
+  const onCancelOrder = async (): Promise<void> => {
+    try {
+      const orderData: IOrderMessage = {
+        sellerId: `${selectedOrder.current?.sellerId}`,
+        buyerId: `${selectedOrder.current?.buyerId}`,
+        purchasedGigs: selectedOrder.current?.gigId
+      };
+      setShowCancelModal(false);
+      await cancelOrder({
+        paymentIntentId: `${selectedOrder.current?.paymentIntent}`,
+        orderId: `${selectedOrder.current?.orderId}`,
+        body: orderData
+      });
+      showSuccessToast('Order cancelled successfully.');
+    } catch (error) {
+      showErrorToast('Error cancelling order. Try again.');
+    }
+  };
 
   return (
     <>
+      {showCancelModal && (
+        <ApprovalModal approvalModalContent={approvalModalContent} onClose={() => setShowCancelModal(false)} onClick={onCancelOrder} />
+      )}
       <div className="flex flex-col">
         <div className="border-grey border border-b-0 px-3 py-3">
           <div className="text-xs font-bold uppercase sm:text-sm md:text-base">{type} orders </div>
@@ -81,7 +110,16 @@ const ManageOrdersTable: FC<IOrderTableProps> = ({ type, orders, orderTypes }): 
                         <Button
                           className="rounded bg-red-500 px-6 py-3 text-center text-sm font-bold text-white focus:outline-none md:px-4 md:py-2 md:text-base"
                           label="Cancel Order"
-                          onClick={() => {}}
+                          onClick={() => {
+                            setApprovalModalContent({
+                              header: 'Order Cancellation',
+                              body: 'Are you sure you want to cancel this order?',
+                              btnText: 'Yes, Cancel',
+                              btnColor: 'bg-red-500 hover:bg-red-400'
+                            });
+                            setShowCancelModal(true);
+                            selectedOrder.current = order;
+                          }}
                         />
                       </td>
                     )}
